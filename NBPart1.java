07d0acad2549704afe6563dc6fec6173dc73c0a5
dearlux <lzhang2@oberlin.edu>
2016-11-04 01:44:52 -0400

    Add files via upload

--- NBPart1.java
+++ NBPart1.java
@@ -1,8 +1,10 @@
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.*;
 
 /**
- * Created by lux on 10/21/2016.
+ * Created by luxing and Yujia on 10/21/2016.
+ * This is a program that uses Naive Bayes to classify labels.
  */
 public class NBPart1 {
     public static void main(String[] args) {
@@ -10,12 +12,20 @@ public class NBPart1 {
         List testing;
         List Att;
         HashMap PLabels;
-        Long seed = System.nanoTime();   //random seed
+        String file;
+        String seed_val;
+        seed_val = args[1];
+//        seed_val = "30";
+        Long seed = Long.parseLong(seed_val);                  //System.nanoTime(); random seed
         Random rng = new Random(seed);
         List shuffled = new ArrayList();
         Integer PercentageSplit = 66;   // percentage of training set
-//        File csvFile = new File("C:/2016 Fall/ML/OpticalDigit.csv");  // file path
-        File csvFile = new File("C:/2016 Fall/ML/monks.csv");  // file path
+        file = args[0];
+//        file = "C:/2016 Fall/ML/opticalDigit.csv";
+        String file_name = file.substring(0, file.lastIndexOf('.'));
+        File f = new File(file);
+        String path = f.getAbsolutePath();
+        File csvFile = new File(path);  // file path
 
         //Scan data
         BufferedReader br = null;
@@ -48,21 +58,24 @@ public class NBPart1 {
         Collections.shuffle(shuffled, rng);
         training = shuffled.subList(0, java.lang.Math.round(shuffled.size() * PercentageSplit / 100));
         testing = shuffled.subList(java.lang.Math.round(shuffled.size() * PercentageSplit / 100), shuffled.size());
-       //run algorithm
+        List unqshuffled = nonrepLabels(shuffled);
+        List unqtraining = nonrepLabels(training);
+        List unqtesting = nonrepLabels(testing);
+        //run algorithm
         NBPart1 nbPart1= new NBPart1();
-        PLabels = nbPart1.PLabels(nbPart1.labelsColumn(training)); //e.g. {"yes"=0.46, "no"=0.53}
+//        PLabels = nbPart1.PLabels(unqtraining); //e.g. {"yes"=0.46, "no"=0.53}
 //        nbPart1.predicted(nbPart1.nonrepLabels(training), Att, testing, training);
 //        nbPart1.calcProb(Att, testing.get(0).toString(), training);
-//        System.out.print("testing"+testing.get(0).toString());
 
 
         //test set
         List actual = nbPart1.labelsColumn(testing);
-        List predicted = nbPart1.predicted(nbPart1.nonrepLabels(testing),Att, testing,training,shuffled);
-        System.out.println(actual);
-        System.out.println(predicted);
+        List predicted = nbPart1.predicted(nonrepattcolumn(shuffled,Att),unqtesting,Att, testing,training,shuffled,unqtraining,training(Att, training,unqtraining),PLabels(labelsColumn(training)));
 
-        int[][] matrix = new int[nbPart1.nonrepLabels(shuffled).size()][nbPart1.nonrepLabels(shuffled).size()];
+        //System.out.println(actual);
+        //System.out.println(predicted);
+
+        int[][] matrix = new int[unqshuffled.size()][unqshuffled.size()];
         for (int m = 0; m < matrix.length; m++) {
             for (int n = 0; n < matrix.length; n++) {
                 matrix[m][n]=0;
@@ -72,43 +85,45 @@ public class NBPart1 {
 
         for(int i=0; i<testing.size();i++){
             int x=0,y=0;
-            for(int j=0; j<nbPart1.nonrepLabels(shuffled).size();j++) {
-                if (actual.get(i).equals(nbPart1.nonrepLabels(shuffled).get(j))) {
+            for(int j=0; j<unqshuffled.size();j++) {
+                if (actual.get(i).equals(unqshuffled.get(j))) {
                     x = j;
                 }
             }
-            for(int j=0; j<nbPart1.nonrepLabels(shuffled).size();j++) {
-                if (predicted.get(i).equals(nbPart1.nonrepLabels(shuffled).get(j))) {
+            for(int j=0; j<unqshuffled.size();j++) {
+                if (predicted.get(i).equals(unqshuffled.get(j))) {
                     y = j;
                 }
             }
             matrix[x][y]+=1;
         }
-        for (int m = 0; m < matrix.length; m++) {
-            for (int n = 0; n < matrix.length; n++) {
-                System.out.print(" ; m "+m+" n "+n+"  "+matrix[m][n]);
-            }
-        }
-        System.out.println(matrix);
+//        for (int m = 0; m < matrix.length; m++) {
+//            for (int n = 0; n < matrix.length; n++) {
+//                System.out.print("m "+m+" n "+n+"  "+matrix[m][n]+"; ");
+//            }
+//        }
+        //System.out.println(matrix);
         //output
+
         PrintWriter pw = null;
         try {
-            pw = new PrintWriter(new File("NBPart1.csv"));
+//            pw = new PrintWriter(new File("results_"+file_name+"_NaiveBayes_"+seed_val+".csv"));
+            pw = new PrintWriter(new File("output.csv"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         StringBuilder sb = new StringBuilder();
-        for(int j=0; j<nbPart1.nonrepLabels(shuffled).size();j++) {
-            sb.append(nbPart1.nonrepLabels(shuffled).get(j));
+        for(int j=0; j<unqshuffled.size();j++) {
+            sb.append(unqshuffled.get(j));
             sb.append(",");
         }
         sb.append("\n");
-        for(int i=0; i<nbPart1.nonrepLabels(shuffled).size();i++){
-            for(int j=0; j<nbPart1.nonrepLabels(shuffled).size();j++) {
+        for(int i=0; i<unqshuffled.size();i++){
+            for(int j=0; j<unqshuffled.size();j++) {
                 sb.append(matrix[i][j]);
                 sb.append(",");
             }
-            sb.append(nbPart1.nonrepLabels(shuffled).get(i));
+            sb.append(unqshuffled.get(i));
             sb.append("\n");
         }
 
@@ -118,39 +133,39 @@ public class NBPart1 {
 
 
 
-    public List predicted(List labels, List Att, List testing, List training, List shuffled){
+    public List predicted(ArrayList<Integer> bs, List labels, List Att, List testing, List training, List shuffled, List unqtraining, HashMap trainingAtttrainingunqtraining,HashMap<String,Double> PLabels){
         List predicted = new ArrayList();
 
         for (int i=0;i<testing.size();i++){
             String label = labels.get(0).toString();
-            Double max = (Double)calcProb(Att, testing.get(i).toString(), training, shuffled).get(0);
+            ArrayList calcprob = calcProb(bs,Att, Arrays.asList((testing.get(i).toString()).split(",")), training, shuffled,unqtraining,trainingAtttrainingunqtraining,PLabels);
+            Double max = (Double)calcprob.get(0);
             for (int j=1;j<labels.size();j++) {
-                if((Double)calcProb(Att, testing.get(i).toString(), training, shuffled).get(j) > max){
-                    max = (Double)calcProb(Att, testing.get(i).toString(), training, shuffled).get(j);
+                if((Double)calcprob.get(j) > max){
+                    max = (Double)calcprob.get(j);
                     label = labels.get(j).toString();
                 }
             }
-        predicted.add(i,label);
+            predicted.add(i,label);
         }
 
         return predicted;
     }
 
     //e.g. [0.0106,0.0274], Prob when label = YES / No
-        public ArrayList calcProb(List Att, String testing, List training, List shuffled){
-            ArrayList<Double> Sum = new ArrayList<>();
-        for (int i=0;i<nonrepLabels(training).size();i++){
+    public ArrayList calcProb(ArrayList<Integer> bs, List Att, List<String> test, List training, List shuffled, List unqtraining, HashMap trainingAtttrainingunqtraining, HashMap<String,Double> PLabels){
+        ArrayList<Double> Sum = new ArrayList<>();
+        for (int i=0;i<unqtraining.size();i++){
             ArrayList<Double> calcProbi = new ArrayList<>();
-            Double PLabel = PLabels(labelsColumn(training)).get(nonrepLabels(training).get(i));  //P(l)
+            Double PLabel = PLabels.get(unqtraining.get(i));  //P(l)
+            HashMap<String, HashMap<String, Double>> temp = (HashMap)trainingAtttrainingunqtraining.get(unqtraining.get(i).toString());
             for (int j=0;j<Att.size();j++){
-                HashMap<String, HashMap<String, Double>> temp = (HashMap)training(Att, training).get(nonrepLabels(training).get(i).toString());
                 HashMap<String, Double> temp2 = (HashMap)temp.get(Att.get(j).toString());
-                List<String> test = Arrays.asList(testing.split(","));
                 Double prob = temp2.get(test.get(j+1).toString());
                 if(prob==null){
                     prob = 0d;
                 }
-                prob = (prob+1)/(temp2.size()+nonrepattcolumn(shuffled,j));
+                prob = (prob+1)/(temp2.size()+bs.get(j));
                 calcProbi.add(prob);
             }
             Double sum = Math.log(PLabel);
@@ -166,20 +181,18 @@ public class NBPart1 {
 
 
     //e.g. key = yes, values = <Outlook,<Sunny,0.2222>>
-    public HashMap training(List Att, List training){
+    public static HashMap training(List Att, List training, List unqtraining){
         HashMap<String, HashMap<String,HashMap<String,Double>>> train = new HashMap<>();
-        List labels =nonrepLabels(training);
+        List labels =unqtraining;
         for (int i=0; i<labels.size();i++){
-            train.put(labels.get(i).toString(), AttProb(labels.get(i).toString(), Att, training));
+            train.put(labels.get(i).toString(), AttProb(Att, subtraining(labels.get(i).toString(), training)));
         }
         return train;
     }
 
     //key = att, value = prob (e.g. <Outlook,<Sunny,0.2222>> )
-    public HashMap AttProb(String label, List Att, List training){
-
+    public static HashMap AttProb( List Att, List subtraining){
         HashMap<String,HashMap<String,Double>> AttProb = new HashMap<>();
-        List subtraining = subtraining(label, training);
         for (int i=0; i<Att.size();i++){
             HashMap<String, Double> Prob = new HashMap<>();
             List attColumn = attColumn(i+1, subtraining);
@@ -198,11 +211,9 @@ public class NBPart1 {
 
 
     //get sub data set
-    public List subtraining(String label, List training){
-//        System.out.print("\n"+"label"+label);
+    public static List subtraining(String label, List training){
         List subtraining=new ArrayList();
         List labelsColumn = labelsColumn(training);
-//        System.out.print("\n"+"labelsColumn"+labelsColumn);
         for (int i=0; i<labelsColumn.size();i++){
             if(labelsColumn.get(i).toString().equals(label)){
                 subtraining.add(training.get(i));
@@ -212,7 +223,7 @@ public class NBPart1 {
     }
 
     //column of ith att  (i=0, label column)
-    public List attColumn(int i, List dataset){
+    public static List attColumn(int i, List dataset){
         List attColumn=new ArrayList(); //labels in S
         for (int j=0; j<dataset.size();j++){
             List<String> s = Arrays.asList((dataset.get(j)).toString().split(","));
@@ -222,7 +233,7 @@ public class NBPart1 {
     }
 
 
-    public List<String> labelsColumn(List dataset){
+    public static List<String> labelsColumn(List dataset){
         List<String> labels = new ArrayList<>(); //labels in S
         for (int i=0; i<dataset.size();i++){
             List<String> s = Arrays.asList((dataset.get(i)).toString().split(","));
@@ -232,8 +243,8 @@ public class NBPart1 {
         return labels;
     }
 
-        //unique labels
-     public List nonrepLabels(List training){
+    //unique labels
+    public static List nonrepLabels(List training){
         List nonrepLabels = new ArrayList();
         HashMap<String, Double> pLabels = PLabels(labelsColumn(training));
         for ( String key : pLabels.keySet() ) {
@@ -243,17 +254,21 @@ public class NBPart1 {
     }
 
     //unique attcolumn
-    public Integer nonrepattcolumn(List training, int i){   //ith att
-        HashSet nonrepattcolumn = new HashSet();
-        List attcol = attColumn(i+1,training);
-        for ( int j =0;j<attcol.size();j++ ) {
-            nonrepattcolumn.add(attcol.get(j));
+    public static ArrayList<Integer> nonrepattcolumn(List shuffled, List Att){   //ith att\
+        ArrayList<Integer> bs = new ArrayList<>();
+        for (int i=0;i<Att.size();i++) {
+            HashSet nonrepattcolumn = new HashSet();
+            List attcol = attColumn(i + 1, shuffled);
+            for (int j = 0; j < attcol.size(); j++) {
+                nonrepattcolumn.add(attcol.get(j));
+            }
+            bs.add(i,nonrepattcolumn.size());
         }
-        return nonrepattcolumn.size();
+        return bs;
     }
 
 
-    public HashMap<String,Double> PLabels(List labels){
+    public static HashMap<String,Double> PLabels(List labels){
         HashMap<String, Double> pLabels = new HashMap<String, Double>();
         for(int i=0;i<labels.size();i++){
             if (!pLabels.containsKey(labels.get(i).toString())){
@@ -262,7 +277,11 @@ public class NBPart1 {
                 pLabels.put(labels.get(i).toString(),pLabels.get(labels.get(i).toString())+1);
             }
         }
-        pLabels.replaceAll((k, v) -> v/labels.size());
+        for (String key : pLabels.keySet()) {
+            Double v = pLabels.get(key);
+            v = v/labels.size();
+            pLabels.put(key, v);
+        }
         return pLabels;
     }
 
